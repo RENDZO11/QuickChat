899b6e3c2d1bca9037f0795d6ef938218a2a0bc5
RENDZO <RENDZO@RENDZOO>
2026-06-14 13:01:21 +0200

    Add QuickChatt messaging application with menu system

--- src/main/java/com/mycompany/quickchat/QuickChat.java
+++ src/main/java/com/mycompany/quickchat/QuickChat.java
@@ -155,7 +155,7 @@ public class QuickChat {
         System.out.println("Total messages sent: " + Message.returnTotalMessages());
         System.out.println("========================================");
     }
- //
+ //Add QuickChatt messaging application with menu system
     // ════════════════════════════════════════════════════════════
     // STORED MESSAGES SUB-MENU  (Part 3 – Requirement 2)
     // ════════════════════════════════════════════════════════════
